ab6a656392795688f4f29eef3c91a45d4359f372
Miles Bryant <milesbryant0@gmail.com>
2015-09-29 01:09:35 +0100

    Refactored move generation and created a superclass of Knight and King.

--- BitChess/src/position/pieces/pieces.hpp
+++ BitChess/src/position/pieces/pieces.hpp
@@ -49,6 +49,7 @@ protected:
 	 */
 	Piece( int initial_sq );
 	bitchess::Bitboard occupancy;
+
 };
 
 //class PieceNone : public Piece {
@@ -63,8 +64,24 @@ protected:
 //class Queen : public Piece {
 //};
 //
-class King: public Piece {
-private:
+
+class SingleMovePiece: protected Piece {
+public:
+	/**
+	 * Gets all pseudolegal moves available to the Piece. Some moves may result in the
+	 * king moving into or being in check, therefore not being legal; these must be checked.
+	 * @return a vector of pseudolegal moves.
+	 */
+	std::vector<bitchess::Move> get_pseudolegal_moves(
+			Bitboard all_occupancy, Bitboard own_occupancy,
+			Bitboard opp_occupancy );
+
+protected:
+	SingleMovePiece() {
+	}
+
+
+
 	/**
 	 * Extracted method scanning moves from a calculated bitboard.
 	 *
@@ -75,14 +92,24 @@ private:
 	 * @param move Reference of Move to modify, typically an array element
 	 * @param is_capture whether the created Move should be a capture
 	 */
-	inline void __scan_moves__(short king_loc,Bitboard& moves_bb, bitchess::Move& move, bool is_capture) {
+	inline void __scan_moves__( short king_loc, Bitboard& moves_bb,
+								bitchess::Move& move, bool is_capture ) {
 		// scan and remove LS1B
 		std::pair<short, Bitboard> result = moves_bb.bitscan_and_remove();
 		int target_sq = result.first; // index of LS1B
 		moves_bb = result.second; // bitboard resulting from bitscan and remove
 		// create a new move at the given reference
-		move = (bitchess::Move(king_loc,target_sq,is_capture,bitchess::PieceType::KING));
+		move = (bitchess::Move(king_loc, target_sq, is_capture,
+							   get_piece_type()));
 	}
+
+	virtual inline bitchess::PieceType::PieceType get_piece_type() = 0;
+
+	virtual inline bitchess::Bitboard get_moves_bitboard(short sq) = 0;
+
+};
+
+class King: public SingleMovePiece {
 public:
 	/**
 	 * Creates a new King with a blank occupancy bitboard.
@@ -99,19 +126,41 @@ public:
 		occupancy = Bitboard(initial_occupancy);
 	}
 
+	inline bitchess::PieceType::PieceType get_piece_type() {
+		return bitchess::PieceType::KING;
+	}
+
+	inline bitchess::Bitboard get_moves_bitboard(short sq) {
+		return bitchess::move_lookup(bitchess::PieceType::KING,sq);
+	}
+};
+
+class Knight: public SingleMovePiece {
+public:
 	/**
-	 * Gets all pseudolegal moves available to the King. Some moves may result in the
-	 * king moving into check, therefore not being legal; these must be checked.
-	 * @return a vector of pseudolegal moves.
+	 * Creates a new Knight with a blank occupancy bitboard.
 	 */
-	virtual std::vector<bitchess::Move> get_pseudolegal_moves(
-			Bitboard all_occupancy, Bitboard own_occupancy,
-			Bitboard opp_occupancy );
+	Knight() {
+
+	}
+
+	/**
+	 * Creates a new Knight with an initial occupancy bitboard.
+	 * @param initial_occupancy
+	 */
+	Knight( Bitboard initial_occupancy ) {
+		occupancy = Bitboard(initial_occupancy);
+	}
+
+	inline bitchess::PieceType::PieceType get_piece_type() {
+		return bitchess::PieceType::KNIGHT;
+	}
+
+	inline bitchess::Bitboard get_moves_bitboard(short sq) {
+		return bitchess::move_lookup(bitchess::PieceType::KNIGHT,sq);
+	}
 
 };
-//
-//class Knight : public Piece {
-//};
 
 class Pawn: public Piece {
 	//TODO: Separate into two more subclasses, BlackPawn and WhitePawn
